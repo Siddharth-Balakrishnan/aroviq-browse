d46511acf70abb6129fa0a6d0fed2bdd517a71db
Siddharth-Balakrishnan <siddharthbalad@gmail.com>
2026-01-19 19:35:54 +0530

    feat: Initialize Spring Boot application with Maven Wrapper and basic project structure.

---
+++ daemon/aroviqd/src/main/java/com/aroviq/aroviqd/AroviqdApplication.java
@@ -0,0 +1,13 @@
+package com.aroviq.aroviqd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroviqdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AroviqdApplication.class, args);
+    }
+
+}
---
+++ daemon/aroviqd/src/test/java/com/aroviq/aroviqd/AroviqdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aroviq.aroviqd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroviqdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
